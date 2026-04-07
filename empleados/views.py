d8763ce48d61d8988a763ad504cacf4d3151405b
Jose Valduz <jose.valduz@unet.edu.ve>
2026-04-07 14:19:05 -0400

    Add views for listing, creating, retrieving, updating, and deleting Empleado instances

--- empleados/views.py
+++ empleados/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Empleado
+from .serializers import EmpleadoSerializer
 
-# Create your views here.
+# Vista para listar todos los empleados y crear uno nuevo (GET, POST)
+class EmpleadoListCreateView(generics.ListCreateAPIView):
+    queryset = Empleado.objects.all().order_by('idEmpleado')
+    serializer_class = EmpleadoSerializer
+
+# Vista para obtener, actualizar o eliminar un empleado específico (GET, PUT, DELETE)
+class EmpleadoRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Empleado.objects.all()
+    serializer_class = EmpleadoSerializer
+    lookup_field = 'idEmpleado' #llave primaria del modelo Empleado
