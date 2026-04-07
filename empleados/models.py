06baf735de88b6b262dfc047b4caf289a07bc9bf
Jose Valduz <jose.valduz@unet.edu.ve>
2026-04-07 07:56:28 -0400

    Add Empleado model with fields for ORM integration

--- empleados/models.py
+++ empleados/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Modelo aplicando el concepto de ORM
+class Empleado(models.Model):
+    idEmpleado = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=150, null=False, blank=False)
+    departamento = models.CharField(max_length=100, null=False, blank=False)
+    sueldo = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = 'empleado' # Fuerza el nombre de la tabla en MySQL
+
+    def __str__(self):
+        return f"{self.nombre} - {self.departamento}"
