35593b881f1be198181e08f1d49760f7318790a2
Jose Valduz <jose.valduz@unet.edu.ve>
2026-04-08 09:23:13 -0400

    Fix URL pattern for Empleado detail view by changing parameter from 'pk' to 'idEmpleado'

--- empleados/urls.py
+++ empleados/urls.py
@@ -4,5 +4,5 @@ from .views import EmpleadoListCreateView, EmpleadoRetrieveUpdateDestroyView #Im
 urlpatterns = [
     path('api/empleados', EmpleadoListCreateView.as_view(), name='empleado-list-create'), #Ruta,vista y nombre de la ruta para listar y crear empleados
     #as_view() es un método que convierte la clase de vista en una función que puede ser llamada por Django cuando se recibe una solicitud HTTP en esa ruta específica. Esto es necesario porque Django espera que las vistas sean funciones, pero al usar clases basadas en vistas, necesitamos convertirlas a funciones para que Django pueda manejarlas correctamente.
-    path('api/empleados/<int:pk>/', EmpleadoRetrieveUpdateDestroyView.as_view(), name='empleado-detail'),
+    path('api/empleados/<int:idEmpleado>', EmpleadoRetrieveUpdateDestroyView.as_view(), name='empleado-detail'),
 ]
